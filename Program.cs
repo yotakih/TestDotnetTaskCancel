606b9ac58f024c1f9f47d7725a5707118590b9f7
yotakih <37173608+yotakih@users.noreply.github.com>
2021-02-23 20:35:52 +0900

    init

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestThread
+{
+    class Program
+    {
+        static CancellationTokenSource cs = null;
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"thread id: {Thread.CurrentThread.ManagedThreadId}");
+            proc();
+            Console.WriteLine("Enter to Task Cancel");
+            Console.ReadLine();
+            cs.Cancel();
+            Console.WriteLine("Please Enter to End");
+            Console.ReadLine();
+        }
+
+        static void proc()
+        {
+            cs = new CancellationTokenSource();
+            var ct = cs.Token;
+            Task.Run(() => {
+                Console.WriteLine($"thread id: {Thread.CurrentThread.ManagedThreadId}");
+                for (var i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"looping: {i+1}");
+                    if (ct.IsCancellationRequested)
+                    {
+                        Console.WriteLine($"Task is stopped");
+                        ct.ThrowIfCancellationRequested();
+                    }
+                    Thread.Sleep(1000);
+                }
+            }, cs.Token);
+        }
+    }
+}
