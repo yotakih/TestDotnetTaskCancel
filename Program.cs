d043a1b7e3e475fa01651a486f15c192d22cd56d
yotakih <37173608+yotakih@users.noreply.github.com>
2021-02-23 21:26:27 +0900

    add throw cancel exception

--- Program.cs
+++ Program.cs
@@ -7,34 +7,77 @@ namespace TestThread
     class Program
     {
         static CancellationTokenSource cs = null;
+        static object cslogobj = new object();
         static void Main(string[] args)
         {
             Console.WriteLine($"thread id: {Thread.CurrentThread.ManagedThreadId}");
+            // var ret = proc().Result;
             proc();
             Console.WriteLine("Enter to Task Cancel");
             Console.ReadLine();
-            cs.Cancel();
+            if (!(cs is null))
+                cs.Cancel();
+            else
+                Console.WriteLine($"Task is already End");
             Console.WriteLine("Please Enter to End");
             Console.ReadLine();
         }
 
-        static void proc()
+        static async Task<int> proc()
         {
             cs = new CancellationTokenSource();
-            var ct = cs.Token;
-            Task.Run(() => {
-                Console.WriteLine($"thread id: {Thread.CurrentThread.ManagedThreadId}");
-                for (var i = 0; i < 10; i++)
+            try
+            {
+                await Task.Run(() =>
                 {
-                    Console.WriteLine($"looping: {i+1}");
-                    if (ct.IsCancellationRequested)
+                    new SubProc(cs).proc();
+                }, cs.Token);
+            }
+            catch (OperationCanceledException e)
+            {
+                Console.WriteLine($"{nameof(OperationCanceledException)} throw with Message: {e.Message}");
+            }
+            finally
+            {
+                cs.Dispose();
+                cs = null;
+            }
+            return 1;
+        }
+    }
+    public class SubProc
+    {
+        public CancellationTokenSource cs = null;
+
+        public SubProc(CancellationTokenSource _cs)
+        {
+            this.cs = _cs;
+        }
+
+        public void proc()
+        {
+            Console.WriteLine($"thread id: {Thread.CurrentThread.ManagedThreadId}");
+            for (var i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"looping: {i + 1}");
+                    if (cs.Token.IsCancellationRequested)
                     {
                         Console.WriteLine($"Task is stopped");
-                        ct.ThrowIfCancellationRequested();
+                        cs.Token.ThrowIfCancellationRequested();
                     }
                     Thread.Sleep(1000);
                 }
-            }, cs.Token);
+                catch (OperationCanceledException e)
+                {
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"catch the error: {e.Message}");
+                }
+            }
         }
     }
 }
